f8e57005c9b6832249dbdf5696c8aae80d13796d
Stuart Nelson <stuartnelson3@gmail.com>
2012-10-18 10:53:08 -0500

    added form to check customer's fq data

--- customer_fq_update.rb
+++ customer_fq_update.rb
@@ -1,11 +1,27 @@
 require 'bundler'
+require 'shotgun'
 require 'sinatra'
 require 'thin'
+require 'shopify_api'
+
+# Get access to Shopify API
+ShopifyAPI::Base.site = "https://63853221c8f1fae9b9b25345b10ec9c8:5ac79471d7d5407d353e015717d6a49b@quincy.myshopify.com/admin"
 
 get "/" do
   erb :index
 end
 
+post "/data" do
+  id = params[:id]
+  @customer = ShopifyAPI::Customer.find(id)
+  @name = @customer.first_name + " " + @customer.last_name
+  @email = @customer.email
+  @bust = @customer.metafields[0].value unless @customer.metafields[0].nil?
+  @quiz_results = @customer.metafields[1].value unless @customer.metafields[1].nil?
+  @length = @customer.metafields[2].value unless @customer.metafields[2].nil?
+  @waist = @customer.metafields[3].value unless @customer.metafields[3].nil?
+  erb :data
+end
 
 
 
